c80baac1001ca2dd38577696bd81a17fa949ecf4
Teelirium <angryzombi01@gmail.com>
2022-03-27 10:44:08 +0500

    components pt 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 import logo from './logo.svg';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className='container'>
-          <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() { 
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <div className='container text-center'>
+          <div className='col-2 offset-5'>
+            This is honestly pretty epic
+          </div>
         </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
